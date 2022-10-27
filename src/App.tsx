67d6d5a3c58ae7d6afc0374d06e936b61ab5a691
Mark Abuyabor <mark@zeguestlist.com>
2022-10-27 23:53:03 +0800

    User interface and styles added on the inputfield component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
@@ -7,6 +8,7 @@ const App: React.FC = () => {
       <h1 className="uppercase text-[35px] md:text-[40px] text-white my-8 md:my-10 z-[1] text-center">
         TODO Board
       </h1>
+      <InputField />
     </div>
   );
 }
