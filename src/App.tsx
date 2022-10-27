8303ca8dd26b283c9021663955ebd1ba138e0816
Mark Abuyabor <mark@zeguestlist.com>
2022-10-27 21:48:34 +0800

    adding font bold in h1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-left text-4xl">
+      <h1 className="text-left text-4xl text-bold">
         Hello World
       </h1> 
     </div>
