286b889b2223801305dfbda5db319704e4b98f70
Mark Abuyabor <mark@zeguestlist.com>
2022-10-27 22:42:05 +0800

    adding the tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1 className="text-left text-4xl text-bold">
-        Hello World
-      </h1> 
+    <div className="App bg-blue-600 w-screen h-screen flex flex-col items-center">
+      <h1 className="uppercase text-[35px] md:text-[40px] text-white my-8 md:my-10 z-[1] text-center">
+        TODO Board
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{jsx,js,tsx,ts}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
