29999b5733b9eb76b34a8c13c56e2f60a77ff78a
Mark Abuyabor <mark@zeguestlist.com>
2022-10-28 14:04:26 +0800

    adding the todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodoList([...todoList, {id: Date.now(), todo, isDone: false} ])
+      setTodo("");
+    }
+  
+  };
+
+  
   return (
     <div className="App bg-blue-600 w-screen h-screen flex flex-col items-center">
       <h1 className="uppercase text-[35px] md:text-[40px] text-white my-8 md:my-10 z-[1] text-center">
         TODO Board
       </h1>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}  />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-const InputField = () => {
+interface Props  {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void
+}
+
+const InputField = ({ todo, setTodo, handleAdd  } : Props) => {
   return (
-    <form className="input flex w-11/12 relative items-center">
+    <form className="input flex w-11/12 relative items-center" onSubmit={handleAdd}>
         <input 
             className="input__box w-full rounded-full py-3 text-xl border-none ease-in duration-300 px-4 shadow-[0_0_10px_1000px_rgba(0,0,0,0.6)] focus:outline-none focus:shadow-2xl" 
             type="input" 
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
             placeholder="Enter a task"  />
         <button className="input__submit absolute my-3 mx-3 rounded-full right-0 border-none text-xl bg-sky-800 text-white transition-all duration-1000 px-4 py-2
-            shadow-xl hover:scale-95" type="submit">
+            shadow-xl hover:scale-95" type="submit" >
             Go
         </button>
     </form>
