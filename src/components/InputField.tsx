c3a057a536baddeb9cea9c4a3059d4605bfaaae8
Mark Abuyabor <mark@zeguestlist.com>
2022-10-28 19:52:53 +0800

    adding the box shadow to small than occupy the whole screen

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -8,15 +8,15 @@ interface Props  {
 
 const InputField = ({ todo, setTodo, handleAdd  } : Props) => {
   return (
-    <form className="input flex w-11/12 relative items-center" onSubmit={handleAdd}>
+    <form className="input flex w-8/12 relative items-center" onSubmit={handleAdd}>
         <input 
-            className="input__box w-full rounded-full py-3 text-xl border-none ease-in duration-300 px-4 shadow-[0_0_10px_1000px_rgba(0,0,0,0.6)] focus:outline-none focus:shadow-2xl" 
+            className="input__box w-full rounded-full py-3 text-xl border-none ease-in duration-300 px-4 shadow-black shadow-2xl focus:outline-none focus:shadow-2xl" 
             type="input" 
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
             placeholder="Enter a task"  />
         <button className="input__submit absolute my-3 mx-3 rounded-full right-0 border-none text-xl bg-sky-800 text-white transition-all duration-1000 px-4 py-2
-            shadow-xl hover:scale-95" type="submit" >
+            shadow-md shadow-black hover:scale-95" type="submit" >
             Go
         </button>
     </form>
