02c27b2c9311b4f31d8d7023093f77af3fcce29f
Mark Abuyabor <mark@zeguestlist.com>
2022-10-30 11:56:30 +0800

    adding the drag to complete functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
@@ -20,9 +20,39 @@ const App = () => {
     }
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination}  = result;
+
+    if(!destination) return;
+
+    if(destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    let add;
+    let active = todoList;
+    let complete = completedTodo;
+
+    if(source.droppableId === "TodosList") {
+      add=active[source.index];
+      active.splice(source.index, 1);
+    } else  {
+      add=active[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if(destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else  {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodo(complete);
+    setTodoList(active);
+
+  }
+
   
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="App bg-emerald-600 w-screen h-screen flex flex-col items-center">
       <h1 className="uppercase text-[35px] md:text-[40px] text-white my-8 md:my-10 z-[1] text-center">
         TODO Board
